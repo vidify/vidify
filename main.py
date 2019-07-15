8ffbda4f7718549d1397cb9b72cd068d2dfbb8c3
Mario Ortiz Manero <marioortizmanero@gmail.com>
2019-07-15 15:42:32 +0200

    Reformatting, adding comments

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 #! /usr/bin/pn
 import os
 import vlc
+import youtube_dl
 import dbus
 import requests
 from datetime import datetime
-from libs import youtube
 # Asynchronous calls
 from dbus.mainloop.glib import DBusGMainLoop
 from gi.repository import GLib
 DBusGMainLoop(set_as_default=True)
 
+
+# DEBUGGING OPTIONS
 debug = True
 
 def log(msg):
@@ -20,13 +22,19 @@ def error(msg):
     print("\033[91m[ERROR]: " + msg + "\033[0m")
 
 
-# Player with dbus and vlc properties
+# YOUTUBE-DL CONFIGURATION
+ydl_opts = {
+    'format' : '136/135/134/133',
+    'outtmpl': 'downloads/%(id)s.%(ext)s',
+    'quiet' : 'true'
+}
+ydl = youtube_dl.YoutubeDL(ydl_opts)
+
+
+# PLAYER CLASS WITH VLC AND DBUS PROPERTIES
 class Player:
     def __init__(self, session_bus, bus_name, connect = True):
-        self._session_bus = session_bus
-        self.bus_name = bus_name
-        self._disconnecting = False
-        self._connect = connect
+        # Main player properties
         self.metadata = {
                 'artist' : '',
                 'title'  : ''
@@ -42,8 +50,12 @@ class Player:
         self.video_player = self._instance.media_player_new()
 
         # DBus internal properties
+        self._session_bus = session_bus
+        self._bus_name = bus_name
+        self._disconnecting = False
+        self._connect = connect
         try:
-            self._obj = self._session_bus.get_object(self.bus_name, '/org/mpris/MediaPlayer2')
+            self._obj = self._session_bus.get_object(self._bus_name, '/org/mpris/MediaPlayer2')
         except dbus.exceptions.DBusException as e:
             error("No spotify session running")
             exit()
@@ -55,19 +67,21 @@ class Player:
         self._loop = GLib.MainLoop()
         self._signals = {}
 
-        self.refreshStatus()
-        self.refreshMetadata()
+        self.refresh_status()
+        self.refresh_metadata()
         if self._connect: self.do_connect()
-
+    
+    # Connects to the dbus signals
     def do_connect(self):
         log("Connecting")
         if self._disconnecting is False:
-            introspect_xml = self._introspect(self.bus_name, '/')
+            introspect_xml = self._introspect(self._bus_name, '/')
             if 'TrackMetadataChanged' in introspect_xml:
-                self._signals['track_metadata_changed'] = self._session_bus.add_signal_receiver(self.refreshMetadata, 'TrackMetadataChanged', self.bus_name)
-            self._properties_interface.connect_to_signal('PropertiesChanged', self.onPropertiesChanged)
+                self._signals['track_metadata_changed'] = self._session_bus.add_signal_receiver(self.refresh_metadata, 'TrackMetadataChanged', self._bus_name)
+            self._properties_interface.connect_to_signal('PropertiesChanged', self.on_properties_changed)
 
-    def disconnect(self):
+    # Disconnects from the dbus signals
+    def do_disconnect(self):
         log("Disconnecting")
         self._disconnecting = True
         for signal_name, signal_handler in list(self._signals.items()):
@@ -89,44 +103,48 @@ class Player:
         log("Stopping loop")
         self._loop.quit()
 
-    def refreshStatus(self):
+    # Refreshes the status of the player (play/pause)
+    def refresh_status(self):
         # Some clients (VLC) will momentarily create a new player before removing it again
         # so we can't be sure the interface still exists
         try:
             self.status = str(self._properties_interface.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus')).lower()
         except dbus.exceptions.DBusException as e:
             error(e)
-            self.disconnect()
+            self.do_disconnect()
 
-    def refreshMetadata(self):
+    # Refreshes the metadata of the player (artist, title)
+    def refresh_metadata(self):
         # Some clients (VLC) will momentarily create a new player before removing it again
         # so we can't be sure the interface still exists
         try:
-            self._assignMetadata()
+            self._assign_metadata()
         except dbus.exceptions.DBusException as e:
             error(e)
-            self.disconnect()
+            self.do_disconnect()
 
     # Assigns the new metadata to the class's properties
-    def _assignMetadata(self):
+    def _assign_metadata(self):
         _metadata = self._properties_interface.Get("org.mpris.MediaPlayer2.Player", "Metadata")
-        self.metadata = self._formatMetadata(_metadata)
+        self.metadata = self._format_metadata(_metadata)
 
-    # Creates an object with raw metadata
-    def _formatMetadata(self, metadata):
+    # Returns a formatted object from raw metadata
+    def _format_metadata(self, metadata):
         return { 'artist' : metadata['xesam:artist'][0], 'title' : metadata['xesam:title'] }
 
-    def formatName(self):
+    # Returns a formatted name with the artist and the title 
+    def format_name(self):
         return self.metadata['artist'] + " - " + self.metadata['title']
 
-    def onPropertiesChanged(self, interface, properties, signature):
+    # Function called asynchronously from dbus on property changes
+    def on_properties_changed(self, interface, properties, signature):
         # Format the new metadata. If it's different, break the loop
         if dbus.String('Metadata') in properties:
-            _new_metadata = self._formatMetadata(properties[dbus.String('Metadata')])
+            _new_metadata = self._format_metadata(properties[dbus.String('Metadata')])
             if _new_metadata != self.metadata:
                 log("New video")
                 self.metadata = _new_metadata
-                self._assignMetadata()
+                self._assign_metadata()
                 self.stop_loop()
         # Paused/Played
         if dbus.String('PlaybackStatus') in properties:
@@ -136,12 +154,14 @@ class Player:
                 self.status = status
                 self.toggle_pause()
 
+    # Plays/Pauses the VLC player
     def toggle_pause(self):
         if self.status == "paused":
             self.video_player.pause()
         else:
             self.video_player.play()
 
+    # Starts a new video on the VLC player
     def start_video(self, filename, offset):
         log("Starting video with offset " + str(offset))
         # Media instance
@@ -153,21 +173,29 @@ class Player:
         if self.status == "playing":
             self.video_player.play()
 
+    # Returns for lyrics from makeitpersonal.co
     def get_lyrics(self):
         pageurl = "https://makeitpersonal.co/lyrics?artist=" + self.metadata['artist'] + "&title=" + self.metadata['title']
         lyrics = requests.get(pageurl).text.strip()
         return lyrics
 
 
+# Download the video with youtube-dl and return the filename
+def download_video(name):
+    info = ydl.extract_info("ytsearch:" + name, download=True)
+    # Fix for prepare_filename inconsistency from youtube-dl
+    return "downloads/" + info['entries'][0]['id'] + ".mp4"
+
+
 # Plays the video until a new song is found
 def main(player):
-    name = player.formatName()
+    name = player.format_name()
     
     # Counts seconds to add a delay and sync the start
     start_time = datetime.now()
     # Only downloading the video if it's not listed
     if name not in player.videos:
-        filename = youtube.download_video(name)
+        filename = download_video(name)
         player.videos[name] = filename
     else:
         filename = player.videos[name]
@@ -181,11 +209,11 @@ def main(player):
 
     # Waiting for the song to finish
     player.wait()
-    log("Finished loop")
 
     main(player)
 
 
+# Player initialization and starting the main function
 if __name__ == '__main__':
     player = Player(
             dbus.SessionBus(),
