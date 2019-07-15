1fc30413c2ef8874c01ef7492f8119186a09f97a
Mario Ortiz Manero <marioortizmanero@gmail.com>
2019-07-15 05:30:56 +0200

    Added README

---
+++ README.md
@@ -0,0 +1,21 @@
+# Spotify Videoclips
+
+A simple tool to show Youtube **videoclips** and **lyrics** for the currently playing Spotify songs with VLC.
+
+![example](screenshots/screenshot.png)
+
+## Dependencies
+
+* `youtube-dl`: used to download the videos since the VLC python API doesn't seem to be able to do it directly
+* `python-vlc`: used to open a pop-up with the videos
+
+## How to install
+
+The project is currently in progress. An installation guide will be provided once it's finished. 
+
+## TODO
+
+* Clean up the vlc module, maybe create a class to access the properties
+* Check [PropertiesChanged](https://dbus.freedesktop.org/doc/dbus-specification.html) rather than the current ugly manual check
+* Use better lyrics API/module
+
--- libs/vlc.py
+++ libs/vlc.py
@@ -16,17 +16,20 @@ def start_video(filename):
     # Player instance
     player.set_media(Media)
     player.set_time(get_current_position())
+    print(get_current_position())
     if get_audio_status() == "Playing":
         player.play()
 
 
 # Accessing properties from dbus
 session_bus = dbus.SessionBus()
-proxy = session_bus.get_object("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
-device_prop = dbus.Interface(proxy, "org.freedesktop.DBus.Properties")
 def get_audio_status():
+    proxy = session_bus.get_object("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
+    device_prop = dbus.Interface(proxy, "org.freedesktop.DBus.Properties")
     return device_prop.Get("org.mpris.MediaPlayer2.Player", "PlaybackStatus")
 def get_current_position():
+    proxy = session_bus.get_object("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
+    device_prop = dbus.Interface(proxy, "org.freedesktop.DBus.Properties")
     return device_prop.Get("org.mpris.MediaPlayer2.Player", "Position")
 
 def toggle_pause(status):
@@ -35,4 +38,3 @@ def toggle_pause(status):
     else:
         player.play()
     
-
