5eab5b4098d0fe9a13a97334af278fe6a40add40
GlowMLG <quiniasol@hotmail.com>
2019-07-21 00:47:40 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='spotify-videoclips',
-    version='1.1',
+    version='1.2',
     packages=find_packages(include=[
         'src', 'src.*'
     ]),
