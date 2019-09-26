44086df4b99a05296bd49fc68ca2d7ef58760269
Mario Ortiz Manero <marioortizmanero@gmail.com>
2019-09-26 21:14:30 +0200

    super() and other small fixes

--- spotivids/gui.py
+++ spotivids/gui.py
@@ -1,14 +1,16 @@
+from typing import Union
+
 from PySide2.QtWidgets import QMainWindow, QWidget
 
 
 class MainWindow(QMainWindow):
-    def __init__(self, widget: QWidget, width: int = 800, height: int = 600):
+    def __init__(self, widget: Union['VLCPlayer', 'MpvPlayer'],
+                 width: int = 800, height: int = 600) -> None:
         """
         Main window with whatever player is being used.
         """
 
-        QMainWindow.__init__(self)
-
+        super().__init__()
         self.setWindowTitle('spotivids')
         self.setStyleSheet('background-color: #282828')
         if None not in (height, width):
