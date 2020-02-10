6dfd3706aeb6b7c71e9120db5595c9cadff7e8b0
Mario Ortiz Manero <marioortizmanero@gmail.com>
2020-02-10 15:59:45 +0100

    2.0.2 - Fix packaging bug

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ else:
 setup(
     name='vidify',
     version=__version__,
-    packages=find_packages(exclude=('tests*')),
+    packages=find_packages(exclude=('tests*', 'dev*')),
     description='Watch music videos for the songs playing on your device',
     long_description=open('README.md', 'r').read(),
     long_description_content_type='text/markdown',
