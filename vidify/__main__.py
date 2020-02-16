ca6eb2ea82d3b31a4f0ecab556b372543cf499b3
Mario O.M <marioortizmanero@gmail.com>
2020-02-16 20:44:23 +0100

    Ignoring stderr from C extensions (closes #55)
    Previously, stderr was ignored only for Python code by replacing
    sys.stderr. If dup2 is used, it's swapped at the file descriptor level,
    meaning that even C extensions won't print to stderr.

--- vidify/__main__.py
+++ vidify/__main__.py
@@ -6,7 +6,6 @@ also initialized here.
 import os
 import sys
 import logging
-from contextlib import redirect_stderr
 
 from qtpy.QtWidgets import QApplication
 from qtpy.QtGui import QIcon
@@ -46,16 +45,15 @@ def main() -> None:
                         format="[%(asctime)s.%(msecs)03d] %(levelname)s:"
                         " %(message)s", datefmt="%H:%M:%S")
 
-    # Redirects stderr to /dev/null if debug is turned off, since sometimes
+    # Redirecting stderr to /dev/null if debug is turned off, since sometimes
     # VLC will print non-fatal errors even when configured to be quiet.
     # This could be redirected to a log file in the future, along with any
     # other output from the logging module.
-    if config.debug:
-        start_gui(config)
-    else:
-        with open(os.devnull, 'wb') as f:
-            with redirect_stderr(f):
-                start_gui(config)
+    if not config.debug:
+        null_fd = os.open(os.devnull, os.O_WRONLY)
+        os.dup2(null_fd, sys.stderr.fileno())
+
+    start_gui(config)
 
 
 if __name__ == '__main__':
