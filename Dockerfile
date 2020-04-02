bf7806db746589cdb9da4c4f61bae38fb32dce0c
Mario Ortiz Manero <marioortizmanero@gmail.com>
2020-04-02 20:36:31 +0200

    Docker integration (#58), automatic Linux builds (#76)
    fix

---
+++ Dockerfile
@@ -0,0 +1,36 @@
+# Multipurpose Dockerfile for tests and building. It installs every single
+# Vidify dependency, even the optionals.
+# I still haven't managed to get it working for tests because Qt requires
+# an open X server, though.
+
+FROM python:3.6
+WORKDIR /vidify/
+# Needed to install programs without interaction
+ENV DEBIAN_FRONTEND=noninteractive
+
+# Apt build dependencies: TODO mpv and pulseaudio unnecessary ??
+RUN apt-get update && apt-get install -y \
+    ffmpeg \
+    gir1.2-gtk-3.0 \
+    libcairo2-dev \
+    libdbus-glib-1-dev \
+    libfftw3-dev \
+    libgirepository1.0-dev \
+    libglib2.0-dev \
+    libmpv-dev \
+    libpulse-dev \
+    libvlc-dev \
+    mpv \
+    pulseaudio \
+    python-gobject \
+    vlc \
+    zip \
+ && rm -rf /var/lib/apt/lists/*
+
+# Python dependencies
+COPY dev/linux_requires.txt .
+RUN pip install -r linux_requires.txt
+
+# The app is ready to be installed
+COPY . .
+RUN pip install . --no-deps
--- dev/build_linux.sh
+++ dev/build_linux.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
+log() { echo -e "\e[36m$*\e[0m"; }
+LIBRARIES=("libmpv.so")
+log "Building Vidify for Linux"
+
 # This temporary folder will be used to modify Vidify's code automatically
 # for different fixes and workarounds.
-echo "Copying module..."
+log "Copying module"
 rm -rf vidify
 cp -r  ../vidify .
 
-version=$(awk '{print $3}' vidify/version.py | tr -d '"')
-
-echo "Applying pre-build patches..."
+log "Applying pre-build patches"
 
 # The issue with Linux building is that there can't be a directory and a file
 # with the same name at once. But both the generated binary and the directory
@@ -25,16 +27,33 @@ sed -i '/RES_DIR = .*/c\import vidify; RES_DIR = os.path.join(os.path.dirname(os
 # correctly.
 echo 'import os; os.environ["VLC_PLUGIN_PATH"] = "/usr/lib64/vlc/plugins"' >> vidify/__init__.py
 
-# Finally running pyinstaller
-echo "Running PyInstaller..."
+
+log "Running PyInstaller"
+
 pyinstaller linux.spec --noconfirm || exit 1
 
-echo "Applying post-build patches..."
+
+log "Applying post-build patches"
+
+# Libraries imported into the files
+for library in "${LIBRARIES[@]}"; do
+    dir=$(find / -name libmpv.so -print -quit)
+    if [ -z "$dir" ]; then
+        log "Library $library not found"
+        exit 1
+    fi
+    log "Found $library at $dir"
+    cp "$dir" dist/vidify
+done
 
 # Required file not installed by default with Tekore.
 mkdir -p dist/vidify/tekore
-wget "https://raw.githubusercontent.com/felix-hilden/tekore/master/tekore/VERSION" -O "dist/vidify/tekore/VERSION"
+wget -q "https://raw.githubusercontent.com/felix-hilden/tekore/master/tekore/VERSION" -O "dist/vidify/tekore/VERSION"
 
-# Saving everything into a zip
-echo "Compressing..."
-zip -r "vidify-${version}_linux_x86_64.zip" dist/vidify 
+# Saving everything into a zip inside dist/
+version=$(awk '{print $3}' vidify/version.py | tr -d '"')
+bin="vidify-v${version}_linux_x86_64.zip"
+log "Compressing into $bin"
+cd dist
+rm -rf "$bin"
+zip -q -r "$bin" vidify/*
