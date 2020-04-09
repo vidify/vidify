8fa86f8e56e7760c0c902c98bc1cab634a26cca2
Mario Ortiz Manero <marioortizmanero@gmail.com>
2020-04-09 03:05:55 +0200

    Windows build fix

--- dev/build_windows.ps1
+++ dev/build_windows.ps1
@@ -17,7 +17,7 @@ echo ">> Applying pre-build patches..."
 # Calling get_distribution at runtime to check if modules are installed
 # doesn't work with PyInstaller, so the is_installed function is overridden:
 # https://github.com/pyinstaller/pyinstaller/issues/4795
-Add-Content ".\vidify\__init__.py" @"
+Add-Content "./vidify/__init__.py" @"
 def is_installed(*args):
     for s in args:
         if s not in ('python-mpv', 'zeroconf', 'swspotify', 'tekore'):
@@ -34,20 +34,20 @@ echo ">> Applying post-build patches..."
 
 # Tekore needs its VERSION file, which is ignored by pyinstaller because
 # it isn't a Python file.
-if (!(Test-Path ".\dist\vidify\tekore\VERSION" -PathType Leaf)) {
-    if (!(Test-Path ".\dist\vidify\tekore")) {
-        New-Item -Path ".\dist\vidify\tekore" -ItemType Directory
+if (!(Test-Path "./dist/vidify/tekore/VERSION" -PathType Leaf)) {
+    if (!(Test-Path "./dist/vidify/tekore")) {
+        New-Item -Path "./dist/vidify/tekore" -ItemType Directory
     }
-    wget "https://raw.githubusercontent.com/felix-hilden/tekore/master/tekore/VERSION" -O ".\dist\vidify\tekore\VERSION"
+    wget "https://raw.githubusercontent.com/felix-hilden/tekore/master/tekore/VERSION" -O "./dist/vidify/tekore/VERSION"
 }
 
 # Including mpv
 if (!(Test-Path mpv-1.dll -PathType Leaf)) {
     wget "$MPV_URL" -O "libmpv.7z" -UserAgent "[Microsoft.PowerShell.Commands.PSUserAgent]::Chrome"
-    7z e libmpv.7z
+    7z e "libmpv.7z"
 }
-cp mpv-1.dll dist/vidify
+cp "mpv-1.dll" "./dist/vidify"
 
 
 echo ">> Compressing..."
-Compress-Archive -Path dist/vidify -Force -DestinationPath "vidify-v${version}_win10_x86_64.zip"
+Compress-Archive -Path "./dist/vidify" -Force -DestinationPath "vidify-v${version}_win10_x86_64.zip"
