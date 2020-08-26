ccfb668f551d301cd1d318abfc22d6dd36ec3d38
Mario Ortiz Manero <marioortizmanero@gmail.com>
2020-08-27 00:54:46 +0200

    Formatted scripts

--- build.py
+++ build.py
@@ -19,13 +19,13 @@ import urllib.request
 
 MPV_DOWNLOAD_WINDOWS = "https://downloads.sourceforge.net/project/mpv-player-windows/libmpv/mpv-dev-x86_64-20200405-git-c5f8ec7.7z?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fmpv-player-windows%2Ffiles%2Flibmpv%2Fmpv-dev-x86_64-20200405-git-c5f8ec7.7z%2Fdownload&ts=1586353127"
 
-IGNORED_FILES = ['.git', '.venv', 'target']
+IGNORED_FILES = [".git", ".venv", "target"]
 
-ALL_OS = ['Linux', 'Windows', 'Darwin']
+ALL_OS = ["Linux", "Windows", "Darwin"]
 
 
 def download_file(uri: str, name: str) -> None:
-    with open(name, 'wb') as output_file:
+    with open(name, "wb") as output_file:
         with urllib.request.urlopen(name) as input_file:
             output_file.write(input_file.read())
 
@@ -61,11 +61,11 @@ logging.basicConfig(level=logging.DEBUG)
 
 # Everything is copied and built in a temporary directory because this
 # script will apply patches to Vidify's source code.
-logging.info('Copying data into build directory')
-shutil.rmtree('build', ignore_errors=True)
-shutil.copytree('.', 'build', ignore=get_ignored)
+logging.info("Copying data into build directory")
+shutil.rmtree("build", ignore_errors=True)
+shutil.copytree(".", "build", ignore=get_ignored)
 
-logging.info('Compiling external libraries')
+logging.info("Compiling external libraries")
 os.chdir("build")
 ret = subprocess.run(["python", "setup.py", "develop"])
 if ret.returncode != 0:
@@ -74,15 +74,19 @@ if ret.returncode != 0:
 
 logging.info("Downloading libraries")
 # Downloading mpv so that it can be embed in the binary
-if cur_os == 'Windows':
+if cur_os == "Windows":
     download_file(MPV_DOWNLOAD_WINDOWS, "libmpv.7z")
     subprocess.run(["7z", "e", "-y", "libmpv.7z"])
     if ret.returncode != 0:
         logging.error("Couldn't extract libmpv.7z")
         exit(1)
 else:
-    ret = subprocess.run(["find", "/", "-name", "libmpv.so", "-print", "-quit"], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-    path = ret.stdout.decode('utf-8').strip()
+    ret = subprocess.run(
+        ["find", "/", "-name", "libmpv.so", "-print", "-quit"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.DEVNULL,
+    )
+    path = ret.stdout.decode("utf-8").strip()
     if len(path) == 0:
         logging.error("Couldn't find libmpv.so")
         exit(1)
@@ -95,21 +99,24 @@ logging.info("Applying pre-build patches")
 logging.info("Running PyInstaller")
 args_os = {
     # The basic information
-    '--name=vidify': ALL_OS,
+    "--name=vidify": ALL_OS,
     # TODO: may not be necessary
     #  '--add-data=tekore/VERSION:tekore/VERSION': ALL_OS,
     # TODO: may not be necessary
-    '--hidden-import=pkg_resources.py2_warn': ALL_OS,
+    "--hidden-import=pkg_resources.py2_warn": ALL_OS,
     # External libraries
     f'--add-data={os.path.join("src", "vidify", "config.cpython*")}:.': ALL_OS,
-    '--add-data=mpv-1.dll': ['Windows'],
-    '--exclude-module=PySide2': ALL_OS,
+    "--add-data=mpv-1.dll": ["Windows"],
+    "--exclude-module=PySide2": ALL_OS,
     os.path.join("src", "vidify", "__main__.py"): ALL_OS,
     os.path.join("src", "vidify", "player", "mpv.py"): ALL_OS,
     os.path.join("src", "vidify", "api", "spotify", "web.py"): ALL_OS,
     os.path.join("src", "vidify", "audiosync.py"): ALL_OS,
-    os.path.join("src", "vidify", "api", "mpris.py"): ['Linux'],
-    os.path.join("src", "vidify", "api", "spotify", "swspotify.py"): ['Windows', 'Darwin'],
+    os.path.join("src", "vidify", "api", "mpris.py"): ["Linux"],
+    os.path.join("src", "vidify", "api", "spotify", "swspotify.py"): [
+        "Windows",
+        "Darwin",
+    ],
 }
 args = filter_os_args(args_os)
 pyinstaller.run(args)
--- setup.py
+++ setup.py
@@ -15,11 +15,11 @@ def is_installed(pkgname: str) -> bool:
 
 # The version should be the same as the one in `Cargo.toml`.
 def get_version():
-    cargo_dir = join(dirname(abspath(__file__)), 'Cargo.toml')
-    with open(cargo_dir, 'r') as f:
+    cargo_dir = join(dirname(abspath(__file__)), "Cargo.toml")
+    with open(cargo_dir, "r") as f:
         for line in f:
             split = line.split()
-            if len(split) > 0 and split[0] == 'version':
+            if len(split) > 0 and split[0] == "version":
                 return split[2][:-1][1:]
 
     raise Exception("Couldn't find `Cargo.toml` version")
@@ -29,102 +29,85 @@ version = get_version()
 
 install_requires = [
     # Base package
-    'QtPy',
-    'lyricwikia',
-    'youtube-dl',
-    'qdarkstyle',
+    "QtPy",
+    "lyricwikia",
+    "youtube-dl",
+    "qdarkstyle",
     'dataclasses; python_version<"3.7"',
-    'vidify_audiosync',
+    "vidify_audiosync",
     # APIs and players
-    'tekore<3.0',
-    'zeroconf',
+    "tekore<3.0",
+    "zeroconf",
     'pydbus; platform_system=="Linux"',
-    'SwSpotify>=1.1.1; platform_system=="Windows"'
-    ' or platform_system=="Darwin"'
+    'SwSpotify>=1.1.1; platform_system=="Windows"' ' or platform_system=="Darwin"',
 ]
 
 # If PySide2 is installed and PyQt5 is not, append PySide2 to dependencies
-if is_installed('PySide2') and not is_installed('PyQt5'):
-    install_requires.append('PySide2')
+if is_installed("PySide2") and not is_installed("PyQt5"):
+    install_requires.append("PySide2")
 # If PySide2 is not installed, or if both PyQt5 and PySide2 are installed
 # Use QtPy's default: PyQt5
 else:
-    install_requires.append('PyQt5')
+    install_requires.append("PyQt5")
 
-extras_require = {
-    'dev': [
-        'flake8',
-        'pyinstaller',
-    ]
-}
+extras_require = {"dev": ["flake8", "pyinstaller",]}
 
 # The Rust extensions will share the same `vidify` namespace as the Python
 # module.
-rust_ext_conf = {
-    'path': 'Cargo.toml',
-    'binding': Binding.PyO3,
-    'debug': False
-}
+rust_ext_conf = {"path": "Cargo.toml", "binding": Binding.PyO3, "debug": False}
 rust_extensions = [
-    RustExtension(
-        "vidify.config",
-        **rust_ext_conf
-    ),
+    RustExtension("vidify.config", **rust_ext_conf),
     # RustExtension(
     #     "vidify.audiosync",
     #     **rust_ext_conf
     # )
 ]
 
-packages = find_packages(exclude=('tests*', 'dev*'))
+packages = find_packages(exclude=("tests*", "dev*"))
 
 # The desktop entry and the icon will be added to Linux systems.
-if platform.startswith('linux'):
-    datafiles = [('share/applications', ['dev/vidify.desktop']),
-                 ('share/pixmaps', ['dev/vidify.svg'])]
+if platform.startswith("linux"):
+    datafiles = [
+        ("share/applications", ["dev/vidify.desktop"]),
+        ("share/pixmaps", ["dev/vidify.svg"]),
+    ]
 else:
     datafiles = []
 
 setup(
     # Metadata for publishing
-    name='vidify',
+    name="vidify",
     version=version,
     packages=packages,
-    description='Watch music videos in real-time for the songs playing on'
-                ' your device',
-    url='https://vidify.org/',
-    license='GPL-3.0+',
+    description="Watch music videos in real-time for the songs playing on"
+    " your device",
+    url="https://vidify.org/",
+    license="GPL-3.0+",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: End Users/Desktop',
-        'Topic :: Multimedia :: Sound/Audio :: Players',
-        'License :: OSI Approved :: GNU Lesser General Public License v3'
-        ' (LGPLv3)',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: End Users/Desktop",
+        "Topic :: Multimedia :: Sound/Audio :: Players",
+        "License :: OSI Approved :: GNU Lesser General Public License v3" " (LGPLv3)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    keywords='spotify music video player videos lyrics linux windows macos',
-
+    keywords="spotify music video player videos lyrics linux windows macos",
     # Data included
     package_dir={"": "src"},
-    long_description=open('README.md', 'r').read(),
-    long_description_content_type='text/markdown',
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
     data_files=datafiles,
-
     # Authors
-    author='Mario Ortiz Manero',
-    author_email='marioortizmanero@gmail.com',
-    maintainer='Mario Ortiz Manero',
-    maintainer_email='marioortizmanero@gmail.com',
-
+    author="Mario Ortiz Manero",
+    author_email="marioortizmanero@gmail.com",
+    maintainer="Mario Ortiz Manero",
+    maintainer_email="marioortizmanero@gmail.com",
     # Metadata for the installation
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     install_requires=install_requires,
     extras_require=extras_require,
-    entry_points={
-        'console_scripts': ['vidify = vidify.__main__:main']
-    },
+    entry_points={"console_scripts": ["vidify = vidify.__main__:main"]},
     rust_extensions=rust_extensions,
     zip_safe=False,  # Rust extensions are not zip safe
 )
